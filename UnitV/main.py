6c45ffbb6a89cf18dd039d6d6601d68b5e458944
mitu <mitus16@outlook.jp>
2025-12-23 16:59:49 +0900

    Fix UnitV bugs

--- UnitV/main.py
+++ UnitV/main.py
@@ -15,13 +15,19 @@ thre_yellow = [(25, 100, -10, 10, 30, 70)]
 task = None
 uart = UART(UART.UART1, 115200,8,0,0, timeout=1000, read_buf_len=4096)
 labels = ("H", "None", "S", "U")
+sensor_window=(224, 224)
+
+# UART codes (1 byte)
+CHAR_CODES = {"U": 1, "S": 2, "H": 3}
+COLOR_CODES = {"G": 4, "Y": 5, "R": 6}
+
+
 
 gc.enable()
 gc.threshold(gc.mem_alloc() + gc.mem_free())
 
 def send(data):
     uart.write(ustruct.pack("B", data))
-    uart.flush()
 
 def detect_character_victim(img):
     global last_character_label
@@ -35,22 +41,28 @@ def detect_character_victim(img):
     label = labels[max_index].strip()
     img.draw_string(0,0, "%.2f : %s" %(pmax, label), scale=2, color=(0, 0, 0))
 
-    if pmax > min_confidence:
-        if last_character_label == label:
-            character_count += 1
-        else:
-            last_character_label = label
-            character_count = 1
-
-        if character_count == 3:
-            character_count = 2
-            if label == "H":
-                max_index = 1
-            return max_index
-    else:
+    if pmax <= min_confidence:
         last_character_label = None
         character_count = 0
-        
+        return None
+
+    if label == "None":
+        last_character_label = None
+        character_count = 0
+        return None
+
+    if last_character_label == label:
+        character_count += 1
+    else:
+        last_character_label = label
+        character_count = 1
+
+    # Debounce: require 3 consecutive frames
+    if character_count == 3:
+        character_count = 2
+        return CHAR_CODES[label]
+    return 0
+
 def detect_colored_victim(img):
     global last_color_label
     global color_count
@@ -62,7 +74,8 @@ def detect_colored_victim(img):
             color_count = 1
         if color_count == 3:
             color_count = 2
-        return 4
+            return COLOR_CODES["R"]
+        return 0
     elif img.find_blobs(thre_green, pixels_threshold=200, area_threshold=200, merge=True):
         if last_color_label == "G":
             color_count += 1
@@ -71,7 +84,8 @@ def detect_colored_victim(img):
             color_count = 1
         if color_count == 3:
             color_count = 2
-        return 5
+            return COLOR_CODES["G"]
+        return 0
     elif img.find_blobs(thre_yellow, pixels_threshold=200, area_threshold=200, merge=True):
         if last_color_label == "Y":
             color_count += 1
@@ -80,22 +94,21 @@ def detect_colored_victim(img):
             color_count = 1
         if color_count == 3:
             color_count = 2
+            return COLOR_CODES["Y"]
+        return 0
     else:
         last_color_label = None
         color_count = 0
-        return 6
+        return None
 
-def main(model_addr=0x300000, sensor_window=(224, 224)):
-    global task
+def main(model_addr=0x300000):
     sensor.reset()
     sensor.set_pixformat(sensor.RGB565)
     sensor.set_framesize(sensor.QVGA)
     sensor.set_windowing(sensor_window)
     sensor.set_auto_exposure(False, 10000)
-    sensor.sleep(2000)
-    sensor.run(1)
-    img = image.Image(size=(320, 240))
 
+    global task
     task = kpu.load(model_addr)
 
     try:
@@ -105,10 +118,12 @@ def main(model_addr=0x300000, sensor_window=(224, 224)):
                 continue
             ch = detect_character_victim(img)
             col = detect_colored_victim(img)
-            if ch is not None:
+            if ch:
                 send(ch)
-            if col is not None:
+                print(ch)
+            elif col:
                 send(col)
+                print(col)
             gc.collect()
     except Exception as e:
         print(e)
